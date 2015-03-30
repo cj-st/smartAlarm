f39be1360d4148edf993a0729a707ebba5073418
xjlim <joelim@alumni.ubc.ca>
2015-03-30 16:29:05 -0700

    remove alarm

--- alarm/smartalarm/smartAlarm.js
+++ alarm/smartalarm/smartAlarm.js
@@ -3,6 +3,8 @@ $(document).ready( function() {
     var currentMode=0;
     var count=0;
     var modeArr;
+    var alarmCount=0;
+    var number=0;
 
     function displayTime() {
         var currentTime = new Date();
@@ -100,43 +102,47 @@ $(document).ready( function() {
     var days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
     option.text="DD/MM/YY";
 
-
     alarmDate.add(option,alarmDate[0]);
 
 
     for(var i=0;i<7;i++){
         var option1=document.createElement("option");
-        option1.text=days[i];
+        var date = new Date();
+        date.setDate(date.getDate() + i);
+        option1.text=days[i]+", "+date.getDate()+'/'+ (date.getMonth()+1) +'/'+date.getFullYear();
         alarmDate.add(option1,alarmDate[i+1]);
     }
 
     var alarmHour=document.getElementById("alarmHour");
     var optionHour=document.createElement("option");
     optionHour.text="hr";
-
-
     alarmHour.add(optionHour,alarmHour[0]);
 
     for(var j=1;j<13;j++){
         var option2=document.createElement("option");
-        option2.text=j;
+        if(j<10){
+            option2.text='0'+j;
+        }else{
+            option2.text=j;
+        }
         alarmHour.add(option2,alarmHour[j]);
     }
-
 
     var alarmMinute=document.getElementById("alarmMinute");
     var optionMinute=document.createElement("option");
     optionMinute.text="min";
 
-
     alarmMinute.add(optionMinute,alarmMinute[0]);
 
-    for(var j=1;j<60;j++){
-        var option2=document.createElement("option");
-        option2.text=j;
-        alarmMinute.add(option2,alarmMinute[j]);
+    for(var k=1;k<60;k++){
+        var option3=document.createElement("option");
+        if(k<10){
+            option3.text='0'+k;
+        }else{
+            option3.text=k;
+        }
+        alarmMinute.add(option3,alarmMinute[k]);
     }
-
 
     var alarmMeridiem=document.getElementById("alarmMeridiem");
     var optionAm=document.createElement("option");
@@ -147,28 +153,54 @@ $(document).ready( function() {
     alarmMeridiem.add(optionPm,alarmMeridiem[1]);
 
 
-
+    var countData=0;
     var messagesRef = new Firebase('https://281alarm.firebaseio.com/');
-    var alarmCount=0;
+    //delete all data once there are 200 of them
+    // messagesRef.on('value',function(data){
+    //     for(var i in data){
+    //         countData++;
+    //     }
+    //     if(countData>200){
+    //         messagesRef.remove();
+    //         counData=0;
+    //     }
+    // });
 
     var set=document.getElementById('setAlarm');
     set.onclick=function(){
-        alarmCount++;
         var alarmdate=document.getElementById('alarmDate').value;
         var alarmhour=document.getElementById('alarmHour').value;
         var alarmminute=document.getElementById('alarmMinute').value;
         var alarmmeridiem=document.getElementById('alarmMeridiem').value;
-        var showAlarm=document.createElement("button");
-        showAlarm.className= "alarms";
-        showAlarm.innerText='Alarm '+alarmCount;
-        document.getElementById("background").appendChild(showAlarm);
-        messagesRef.push({date:alarmdate, hour:alarmhour, minute:alarmminute, meridiem:alarmmeridiem});
+        if(alarmdate!='DD/MM/YY'&&alarmhour!='hr'&&alarmminute!='min'){
+
+           if(number <= 10) {
+
+            var childRef = messagesRef.child("alarm" + (number+1));
+            childRef.set({id:number+1,date:alarmdate, hour:alarmhour, minute:alarmminute, meridiem:alarmmeridiem});
+
+            //alert fails when there are too many data
+        }else { alert('maximum alarm set')};
+        }else { alert('please select a day and time')};
     };
-//     if(alarmCount>0){
-//     var getAlarm=document.getElementById("background").getElementsByClassName("alarms");
-//     console.log(getAlarm);
-// }
-    // getAlarm.onclick=function(){
-    //     alert('hi');
-    // };
+
+    messagesRef.on('child_added',function(snapshot){
+        number++;
+        var showAlarm=document.createElement("button");
+        showAlarm.id= number;
+        showAlarm.innerText=snapshot.val().date+" "+snapshot.val().hour+":"+snapshot.val().minute;
+        document.getElementById("background").appendChild(showAlarm);
+        showAlarm.onclick=function(){
+
+            var childRef = messagesRef.child('alarm' + showAlarm.id);
+            childRef.remove();
+            // Get the child element node
+            var child = document.getElementById(showAlarm.id);
+            // Remove the child element from the document
+            child.parentNode.removeChild(child);
+
+            };
+
+    });
+
 });
