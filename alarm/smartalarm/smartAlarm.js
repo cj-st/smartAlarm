a2ff9a627a1ada5e33b3fe0a249a7d35ba44b8e8
xjlim <joelim@alumni.ubc.ca>
2015-03-29 21:06:25 -0700

    update firebase

--- alarm/smartalarm/smartAlarm.js
+++ alarm/smartalarm/smartAlarm.js
@@ -4,15 +4,6 @@ $(document).ready( function() {
     var count=0;
     var modeArr;
 
-
-
-    //This code will run after your page loads
-    // var GoogleMapsLoader = require('google-maps');      // only for common js environments
-
-    // GoogleMapsLoader.load(function(google) {
-    //     new google.maps.Map(el, options);
-    // });
-
     function displayTime() {
         var currentTime = new Date();
         var hours = currentTime.getHours();
@@ -55,7 +46,11 @@ $(document).ready( function() {
         success: function (data) {
             var icon=data.currently.icon;
             var temp=(data.currently.temperature-32)/1.8;
+            var background = document.getElementById('background');
+            clockDiv = document.getElementById('clock');
             weatherDiv=document.getElementById('weather');
+            weatherDiv=document.getElementById('weather');
+
             weatherDiv.innerText="Currently "+ icon +" " + Math.round(temp)+" °C";
             switch(icon){
             case 'clear-day':
@@ -95,99 +90,11 @@ $(document).ready( function() {
         }
     });
     }
-    // function weather(){
-    //     $.get("https://api.forecast.io/forecast/c041d01ad874a1877cd9917877f38154/37.8267,-122.423", function(data, status){
-    //     var icon=data.weather[0].icon;
-    //     var description=data.weather[0].description;
-    //     var temp=data.main.temp-273.15;
-    //     var background = document.getElementById('background');
-    //     clockDiv = document.getElementById('clock');
-    //     weatherDiv=document.getElementById('weather');
-    //     weatherDiv.innerText="Currently "+ description +" " + Math.round(temp)+" °C";
-    //     switch(icon){
-    //         case '01d':
-    //         case '02d':
-    //             background.style.backgroundImage='url("sunny.jpg")';
-    //             break;
-    //         case '03d':
-    //         case '04d':
-    //         case '03n':
-    //         case '04n':
-    //             background.style.backgroundImage='url("cloudy.jpg")';
-    //             clockDiv.style.color='white';
-    //             weatherDiv.style.color='white';
-    //             break;
-    //         case '09d':
-    //         case '10d':
-    //         case '09n':
-    //         case '10n':
-    //             background.style.backgroundImage='url("rainyDay.jpg")';
-    //             clockDiv.style.color='white';
-    //             weatherDiv.style.color='white';
-    //             break;
-    //         case '11d':
-    //         case '11n':
-    //             background.style.backgroundImage='url("thunder.jpg")';
-    //             clockDiv.style.color='white';
-    //             weatherDiv.style.color='white';
-    //             break;
-    //         case '13d':
-    //         case '13n':
-    //             background.style.backgroundImage='url("snowy.jpg")';
-    //             break;
-    //         case '50d':
-    //         case '50n':
-    //             background.style.backgroundImage='url("mist.jpg")';
-    //             break;
-    //         case '01n':
-    //         case '02n':
-    //             background.style.backgroundImage='url("clearNight.jpg")';
-    //             weatherDiv.style.color='white';
-    //             clockDiv.style.color='white';
-    //             break;
-    //     }
 
-    //     // ="Today weather is "+ description +".\nHigh:"+tempHigh+"Low:"+tempLow;
-    //     });
-    // }
     weather();
-    // modeArr=[
-    //     print,
-    //     weather,
-    //     setInterval
-    // ];
 
-    //run clock from start
     var auto=setInterval(displayTime,1000);
 
-    //bad model of running continuous event in onclick(set interval run function at every x ms, clear manually )
-    //huge delay
-    // var mode=document.getElementById('modes');
-    // mode.onclick=function() {
-
-    //     if(modeArr[0]==print){
-    //         currentMode=1;
-    //     }else if(modeArr[0]==weather)
-    //     {
-    //         currentMode=2;
-    //     }else{
-    //         currentMode=0;
-    //     }
-    //     switch(currentMode){
-    //     case 1:
-    //         clearInterval(auto);
-    //         print();
-    //         break;
-    //     case 2:
-    //         clearInterval(auto);
-    //         weather();
-    //         break;
-    //     default:
-    //         auto=setInterval(displayTime,1000);
-    //     }
-
-    //     modeArr.push(modeArr.splice(0,1)[0]);   //sequential loop
-    // };
     var alarmDate=document.getElementById("alarmDate");
     var option=document.createElement("option");
     var days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
@@ -238,19 +145,30 @@ $(document).ready( function() {
     var optionPm=document.createElement("option");
     optionPm.text="pm";
     alarmMeridiem.add(optionPm,alarmMeridiem[1]);
+
+
+
+    var messagesRef = new Firebase('https://281alarm.firebaseio.com/');
+    var alarmCount=0;
+
     var set=document.getElementById('setAlarm');
-
-    //just write something to the end of the page
     set.onclick=function(){
-        var showAlarm=document.createElement("div");
-        document.body.appendChild(showAlarm);
-        var alarms=new Object();
-        alarms.date=document.getElementById('alarmDate').selectedIndex;
-        alarms.hour=document.getElementById('alarmHour').selectedIndex;
-        alarms.minute=document.getElementById('alarmMinute').selectedIndex;
-        alarms.meridiem=document.getElementById('alarmMeridiem').selectedIndex;
-        showAlarm.innerText=JSON.stringify(alarms);
+        alarmCount++;
+        var alarmdate=document.getElementById('alarmDate').value;
+        var alarmhour=document.getElementById('alarmHour').value;
+        var alarmminute=document.getElementById('alarmMinute').value;
+        var alarmmeridiem=document.getElementById('alarmMeridiem').value;
+        var showAlarm=document.createElement("button");
+        showAlarm.className= "alarms";
+        showAlarm.innerText='Alarm '+alarmCount;
+        document.getElementById("background").appendChild(showAlarm);
+        messagesRef.push({date:alarmdate, hour:alarmhour, minute:alarmminute, meridiem:alarmmeridiem});
     };
-    //$('alarm').onclick=
+//     if(alarmCount>0){
+//     var getAlarm=document.getElementById("background").getElementsByClassName("alarms");
+//     console.log(getAlarm);
+// }
+    // getAlarm.onclick=function(){
+    //     alert('hi');
+    // };
 });
-
