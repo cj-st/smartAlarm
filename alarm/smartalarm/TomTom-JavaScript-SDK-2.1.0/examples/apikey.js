25d836021b31915256b7ce1d47fe48796961484e
xjlim <joelim@alumni.ubc.ca>
2015-04-05 17:05:41 -0700

    added login removed unused files

--- alarm/smartalarm/TomTom-JavaScript-SDK-2.1.0/examples/apikey.js
+++
@@ -1,5 +0,0 @@
-// enter your API key here
-tomtom.apiKey = "pw7q86ngkxnqw37exmyrxsje";
-
-if (!tomtom.apiKey)
-	alert("Please edit apikey.js and enter your API key. The examples will not function until an API key has been entered.");
